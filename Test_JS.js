404cc5ad422417362ebe9464d707b997e5a788b3
TimKroeger13 <t.kroeger13@googlemail.com>
2022-07-05 12:22:52 +0200

    reset

--- Test_JS.js
+++ Test_JS.js
@@ -9,15 +9,7 @@ function AddOneLocal() {
     }
 
 
-    var file = document.getElementById("uploadfile").value;
-    $.post('localhost:22222', //this is where I am hosting the server
-        { SendData: file },
-        function (returnedArray) {
-            var newData = //the response from python server, in array format;
-                //set up different variables from the array
-                //update the returned_info div with the new data
-            }
-    )
+
 
 }
 
